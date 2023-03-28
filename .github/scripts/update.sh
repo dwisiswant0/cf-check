6ad024715be02cd245cef9d9c1ac2d860261b132
Dwi Siswanto <me@dw1.io>
2023-03-28 18:53:52 +0700

    ci(script): add sort cmd

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -2,7 +2,8 @@
 
 function getCfPrefs() {
   curl -s "https://api.bgpview.io/asn/${1}/prefixes" | \
-    jq -r '.data.ipv4_prefixes[].parent.prefix' | grep /
+    jq -r '.data.ipv4_prefixes[].parent.prefix' | \
+    grep / | sort -u
 }
 
 DB="$(echo -e "$(getCfPrefs "13335")\n$(getCfPrefs "395747")" | sort -u)"
